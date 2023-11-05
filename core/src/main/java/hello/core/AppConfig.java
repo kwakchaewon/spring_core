44c6af9627eb1cf34462858a6f6234e080b7498b
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-11-05 14:25:53 +0900

    AppCounfig 리팩토링
    - new MemoryMemberRepository() 이 부분이 중복 제거
    - 역할과 구현 클래스가 한눈에. 코드의 가독성 증가

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,13 +12,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
     
     // memberService 가 생성될 때 MemberServiceImpl 가 주입되고 MemoryMemberRepository 가 파라미터로 주입됨
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
     }
-
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
