2c0f2e90fa1617952dfd0c5a5e85edab313f8357
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-11-06 15:57:57 +0900

    스프링 중복 타입의 빈 조회

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,68 @@
+package hello.core.beanfind;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+
+import java.util.Map;
+
+public class ApplicationContextSameBeanFindTest {
+
+    // 2. 클래스 내 스프링 컨테이너 가져오기
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생")
+    void findBeanByTypeDuplicate() {
+        
+        // 아래 코드 실행 시, 같은 타입이 2개라 NoUniqueBeanDefinitionException 발생
+        //MemberRepository bean = ac.getBean(MemberRepository.class);
+
+        assertThrows(NoUniqueBeanDefinitionException.class, () ->
+                ac.getBean(MemberRepository.class));
+    }
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 빈 이름을 지정")
+    void findBeanByName() {
+        
+        // 파라미터로 빈 이름 지정
+        MemberRepository memberRepository = ac.getBean("memberRepository1",
+                MemberRepository.class);
+        assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " +
+                    beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+    
+    // 1. 클래스 안에서만 사용할 Config 클래스 생성
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+
+}
